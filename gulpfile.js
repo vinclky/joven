2527bc22991283ef047a6e4b3f2ac97686928a0e
Unknown <jazieltan@gmail.com>
2018-10-06 17:59:47 +0800

    Gulp update
    JS courcemaps etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,32 @@
 /**
  * Jaziel's Gulp Automation
- * Joven Website
  */
 
-// var gulpAutoprefixer = require("gulp-autoprefixer");
-var autoprefixer = require("autoprefixer");
+var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
-// var csscomb = require("gulp-csscomb");
+var csscomb = require("gulp-csscomb");
 var cleanCss = require("gulp-clean-css");
-// var cache = require('gulp-cache');
-// var cssnano = require('gulp-cssnano');
+var cache = require("gulp-cache");
+var cssnano = require("gulp-cssnano");
 var del = require("del");
-// var imagemin = require('gulp-imagemin');
-// var htmlPrettify = require('gulp-html-prettify');
+var imagemin = require("gulp-imagemin");
+var htmlPrettify = require("gulp-html-prettify");
 var gulp = require("gulp");
-// var gulpIf = require('gulp-if');
-// var gulpRun = require('gulp-run');
-// var npmDist = require('gulp-npm-dist');
+var gulpIf = require("gulp-if");
+var gulpRun = require("gulp-run");
+var gulpUtil = require("gulp-util");
+var npmDist = require("gulp-npm-dist");
 var postcss = require("gulp-postcss");
+var plumber = require("gulp-plumber");
 var runSequence = require("run-sequence");
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
-var postcssFlexBug = require("postcss-flexbugs-fixes");
-// var useref = require('gulp-useref-plus');
-// var wait = require("gulp-wait");
+var replace = require("gulp-replace");
+var saveLicense = require("uglify-save-license");
+var useref = require("gulp-useref-plus");
+var wait = require("gulp-wait");
 
 // Define paths
 
@@ -51,50 +52,22 @@ var paths = {
 };
 
 // Compile SCSS
-gulp.task("scss", function() {
-  return (
-    gulp
-      .src(paths.src.scss)
-      .pipe(sourcemaps.init())
-      .pipe(sass().on("error", sass.logError))
-      // .pipe(postcss([require(postcssFlexBug)]))
-      .pipe(postcss([autoprefixer({ browsers: ["> 1%"] })]))
-      // .pipe(csscomb())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(paths.src.css))
-      .pipe(
-        browserSync.reload({
-          stream: true
-        })
-      )
-  );
-});
 
-// Minify CSS
-gulp.task("minify:css", function() {
+gulp.task("scss", function () {
   return gulp
-    .src([paths.src.css + "/theme.css"])
-    .pipe(cleanCss())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest(paths.dist.base + "/assets/css"));
-});
-
-// Minify JS
-gulp.task("minify:js", function(cb) {
-  return gulp
-    .src([paths.src.base + "/assets/js/theme.js"])
-    .pipe(uglify())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest(paths.dist.base + "/assets/js"));
-});
-
-//generate sourcemaps JS
-gulp.task("sourcemap:js", function() {
-  return gulp
-    .src([paths.src.base + "/assets/js/theme.js"])
+    .src(paths.src.scss)
+    .pipe(wait(500))
     .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.src.js))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([require("postcss-flexbugs-fixes")]))
+    .pipe(
+      autoprefixer({
+        browsers: ["> 1%"]
+      })
+    )
+    .pipe(csscomb())
+    .pipe(sourcemaps.write("maps"))
+    .pipe(gulp.dest(paths.src.css))
     .pipe(
       browserSync.reload({
         stream: true
@@ -102,8 +75,35 @@ gulp.task("sourcemap:js", function() {
     );
 });
 
+// Minify CSS
+
+gulp.task("minify:css", function () {
+  return gulp
+    .src(paths.src.css + "/theme.css")
+    .pipe(sourcemaps.init())
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write("maps"))
+    .pipe(gulp.dest(paths.dist.base + "/css"));
+});
+
+// Minify JS
+
+gulp.task("minify:js", function (cb) {
+  return gulp
+    .src(paths.src.base + "/assets/js/theme.js")
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write("maps"))
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write("maps"))
+    .pipe(gulp.dest(paths.dist.base + "/js"));
+});
+
 // Live reload
-gulp.task("browserSync", function() {
+
+gulp.task("browserSync", function () {
   browserSync.init({
     server: {
       baseDir: [paths.src.base, paths.base.base]
@@ -112,59 +112,59 @@ gulp.task("browserSync", function() {
 });
 
 // Watch for changes
-gulp.task("watch", ["browserSync", "scss"], function() {
+
+gulp.task("watch", ["browserSync", "scss"], function () {
   gulp.watch(paths.src.scss, ["scss"]);
   gulp.watch(paths.src.js, browserSync.reload);
   gulp.watch(paths.src.html, browserSync.reload);
 });
 
 // Clean
-gulp.task("clean:dist", function() {
+
+gulp.task("clean:dist", function () {
   return del.sync(paths.dist.base);
 });
 
 // Copy CSS
-gulp.task("copy:css", function() {
+
+gulp.task("copy:css", function () {
   return gulp
     .src([paths.src.base + "/assets/css/theme.css"])
-    .pipe(gulp.dest(paths.dist.base + "/assets/css"));
-});
-
-// Copy JS
-gulp.task("copy:js", function() {
-  return gulp
-    .src([paths.src.base + "/assets/js/theme.js"])
-    .pipe(gulp.dest(paths.dist.base + "/assets/js"));
+    .pipe(gulp.dest(paths.dist.base + "/css"));
 });
 
 //Copy all images and directories
+
 gulp.task("copy:img", function() {
   return gulp
     .src([paths.src.base + "/assets/img/**/*"])
     .pipe(gulp.dest(paths.dist.base + "/assets/img/"));
 });
 
-// copy all used html
+ // copy all used html
+
 gulp.task("copy:html", function() {
-  return gulp.src([paths.src.base + "*.html"]).pipe(gulp.dest(paths.dist.base));
+  return gulp.src([paths.src.base + "*.html"])
+    .pipe(gulp.dest(paths.dist.base));
 });
 
 // Build
-gulp.task("build", function(callback) {
+
+gulp.task("build", function (callback) {
   runSequence(
     "clean:dist",
     "scss",
     "copy:css",
-    "copy:js",
-    "copy:img",
-    "copy:html",
     "minify:js",
     "minify:css",
+    "copy:img",
+    "copy:html",
     callback
   );
 });
 
 // Default
-gulp.task("default", function(callback) {
+
+gulp.task("default", function (callback) {
   runSequence(["scss", "browserSync", "watch"], callback);
 });
